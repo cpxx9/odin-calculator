fe0a10db0e04d1507a8b49e486c73386188a9627
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-10-21 12:25:54 -0400

    pushed changes

---
+++ JS/scripts.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(first, last, operator) {
+    if(operator === 'add') {
+        return add(first, last);
+    } if(operator === 'subtract') {
+        return subtract(first, last);
+    }if(operator === 'multiply') {
+        return multiply(first, last);
+    } if(operator === 'divide') {
+        return divide(first, last);
+    } 
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
