4c38654fc44ab7a555ff54ad2a316503b20abe21
Cole Pratte <127376939+cpxx9@users.noreply.github.com>
2023-12-19 13:52:44 -0500

    buttons output their respective numbers

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", function()) {
-  let clear = 
+//global variables
+let operator = '';
+let previousValue = '';
+let currentValue = '';
+
+//store DOM content into variables AFTER fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+  let clear = document.querySelector("#clear");
+  let equal = document.querySelector("#equals");
+  let decimal = document.querySelector("#decimal");
+
+  let numbers = document.querySelectorAll(".numbers");
+  let operators = document.querySelectorAll(".operators")
+
+  let previousScreen = document.querySelector(".previous");
+  let currentScreen = document.querySelector(".current");
+
+  numbers.forEach((number) => number.addEventListener("click", function (e) {
+    handleNumber(e.target.textContent)
+  }));
+})
+
+function handleNumber(num) {
+  console.log(num);
 }
